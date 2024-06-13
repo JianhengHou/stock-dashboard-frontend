1ab7cae21eaae505ebed03ed08ddd460d087e079
Jianheng Hou <jianhenghou@Jianhengs-Laptop.local>
2024-06-13 02:15:35 -0700

    upload candlechart

---
+++ src/components/Charts/ChartFour.tsx
@@ -0,0 +1,172 @@
+import { ApexOptions } from 'apexcharts';
+import React, { useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const candleOptions: ApexOptions = {
+  chart: {
+    type: 'candlestick',
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  title: {
+    text: 'CandleStick Chart',
+    align: 'left',
+  },
+  xaxis: {
+    type: 'datetime',
+  },
+  yaxis: {
+    tooltip: {
+      enabled: true,
+    },
+  },
+  plotOptions: {
+    candlestick: {
+      colors: {
+        upward: '#3C50E0',
+        downward: '#80CAEE',
+      },
+      wick: {
+        useFillColor: true,
+      },
+    },
+  },
+  tooltip: {
+    enabled: true,
+  },
+  responsive: [
+    {
+      breakpoint: 1024,
+      options: {
+        chart: {
+          height: 300,
+        },
+      },
+    },
+    {
+      breakpoint: 1366,
+      options: {
+        chart: {
+          height: 350,
+        },
+      },
+    },
+  ],
+};
+
+interface CandleChartState {
+  series: {
+    name: string;
+    data: {
+      x: Date;
+      y: [number, number, number, number];
+    }[];
+  }[];
+}
+
+const CandleChart: React.FC = () => {
+  const [state, setState] = useState<CandleChartState>({
+    series: [
+      {
+        name: 'Candlestick',
+        data: [
+          {
+            x: new Date(1538778600000),
+            y: [6629.81, 6650.5, 6623.04, 6633.33],
+          },
+          {
+            x: new Date(1538780400000),
+            y: [6632.01, 6643.59, 6620, 6630.11],
+          },
+          {
+            x: new Date(1538782200000),
+            y: [6630.71, 6648.95, 6623.34, 6635.65],
+          },
+          {
+            x: new Date(1538784000000),
+            y: [6635.65, 6651, 6629.67, 6638.24],
+          },
+          {
+            x: new Date(1538785800000),
+            y: [6638.24, 6640, 6620, 6624.47],
+          },
+          {
+            x: new Date(1538787600000),
+            y: [6624.53, 6636.03, 6621.68, 6624.31],
+          },
+          {
+            x: new Date(1538789400000),
+            y: [6624.61, 6632.2, 6617, 6626.02],
+          },
+          {
+            x: new Date(1538791200000),
+            y: [6627, 6627.62, 6584.22, 6603.02],
+          },
+          {
+            x: new Date(1538793000000),
+            y: [6605, 6608.03, 6598.95, 6604.01],
+          },
+          {
+            x: new Date(1538794800000),
+            y: [6604.5, 6614.4, 6602.26, 6608.02],
+          },
+        ],
+      },
+    ],
+  });
+
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
+      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
+        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
+          <div className="flex min-w-47.5">
+            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
+            </span>
+            <div className="w-full">
+              <p className="font-semibold text-primary">Total Revenue</p>
+              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
+            </div>
+          </div>
+          <div className="flex min-w-47.5">
+            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
+            </span>
+            <div className="w-full">
+              <p className="font-semibold text-secondary">Total Sales</p>
+              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full max-w-45 justify-end">
+          <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
+            <button className="rounded bg-white py-1 px-3 text-xs font-medium text-black shadow-card hover:bg-white hover:shadow-card dark:bg-boxdark dark:text-white dark:hover:bg-boxdark">
+              Day
+            </button>
+            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
+              Week
+            </button>
+            <button className="rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark">
+              Month
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div id="candleChart" className="-ml-5">
+          <ReactApexChart
+            options={candleOptions}
+            series={state.series}
+            type="candlestick"
+            height={350}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CandleChart;
